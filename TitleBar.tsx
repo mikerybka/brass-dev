ea7866ae8625c59235394a40aee31a38fd916ca6
Mike Rybka <merybka@gmail.com>
2025-07-07 16:11:17 -0400

    wip

--- TitleBar.tsx
+++ TitleBar.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import popPath from './popPath';
+import ActionButton from './ActionButton';
 
 export default function TitleBar(props: {
     path: string;
 }) {
     return <div className="border flex justify-between">
         <div className="text-xl font-bold">{props.path}</div>
+        <ActionButton className='border' title='DELETE' description='Delete this object.' fields={[]} method='DELETE' path={props.path} onSuccess={(body: string)=> {
+            console.log(body)
+        }}/>
         <button onClick={() => {
             fetch(props.path, {
                 method: "DELETE",
