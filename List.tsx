714cad0d2099769ad3ae576100fe757974a41b05
Mike Rybka <merybka@gmail.com>
2025-07-08 09:27:48 -0400

    wip

--- List.tsx
+++ List.tsx
@@ -4,8 +4,7 @@ import Button from './Button';
 export default function List(props: { title: string; onCreate: () => void; children: React.ReactNode }) {
     return <div className="bg-gray-200">
         <div>{props.title}</div>
-        <div className="bg-white">
-
+        <div className="bg-white m-2">
             {props.children}
             <Button onClick={props.onCreate}>+</Button>
         </div>
