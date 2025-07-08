f9cee79948ccc95b779be527ee2a93a1b216cd7f
Mike Rybka <merybka@gmail.com>
2025-07-08 09:23:58 -0400

    wip

--- StringInput.tsx
+++ StringInput.tsx
@@ -4,8 +4,11 @@ export default function StringInput(props: {
     value: string;
     onChange: (s: string) => void;
 }) {
-    return <div>
-        <div>{props.label}:</div>
-        <input type='text' value={props.value} onChange={e => props.onChange(e.target.value)} />
+    return <div className="w-full max-w-sm mx-auto mt-4">
+        <label className="block text-sm font-medium text-gray-700 mb-1">
+            {props.label}
+        </label>
+        <input type='text' className="w-full px-4 py-2 border border-gray-300 rounded-xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            value={props.value} onChange={e => props.onChange(e.target.value)} />
     </div>
 }
