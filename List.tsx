2652df60c05e3958a69e3702aca26ab2ac5efe13
Mike Rybka <merybka@gmail.com>
2025-07-08 13:43:33 -0400

    wip

--- List.tsx
+++ List.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function List(props: { title: string; onCreate: () => void; children: React.ReactNode }) {
-    return <div className=" p-2">
+    return <div className="bg-white p-2">
         <div className="font-bold">{props.title}</div>
         <div className="bg-white">
             {props.children}
